89d86f4d968c77475b3d248e5ffca4735691e210
Yan Andrey <43590571+yanandrey@users.noreply.github.com>
2024-06-08 19:12:14 -0300

    simple flask api

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
-from flask import Flask, request    
-from flask_restful import Resource, Api
+from flask import Flask, request
+from config import Config
+from models import db, User
+from flask_restx import Api, Resource, fields
 
-app = Flask(__name__)
-api = Api(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-fakeDatabase = {
-    1:{'name':'Clean car'},
-    2:{'name':'Write blog'},
-    3:{'name':'Start stream'},
-}
+    db.init_app(app)
 
-class Items(Resource):
-    def get(self):
-        return fakeDatabase
+    with app.app_context():
+        db.create_all()
 
-    def post(self):
-        data = request.json
-        itemId = len(fakeDatabase.keys()) + 1
-        fakeDatabase[itemId] = {'name':data['name']}
-        return fakeDatabase
+    api = Api(app, version='1.0', title='RAD API', description='A simple RAD API', doc='/swagger/')
 
-class Item(Resource):
-    def get(self, pk):
-        return fakeDatabase[pk]
+    ns = api.namespace('users', description='RAD with Python')
 
-api.add_resource(Items, '/')
-api.add_resource(Item, '/<int:pk>')
+    user_model = api.model('User', {
+        'id': fields.Integer(readOnly=True, description='User unique identifier'),
+        'name': fields.String(required=True, description='User name'),
+        'email': fields.String(required=True, description='User email')
+    })
+
+    @ns.route('/')
+    class UserList(Resource):
+        @ns.doc('list_users')
+        @ns.marshal_list_with(user_model)
+        def get(self):
+            """List all users"""
+            users = User.query.all()
+            return users
+
+        @ns.doc('create_user')
+        @ns.expect(user_model)
+        @ns.marshal_with(user_model, code=201)
+        def post(self):
+            """Create a new user"""
+            data = request.json
+            new_user = User(name=data['name'], email=data['email'])
+            db.session.add(new_user)
+            db.session.commit()
+            return new_user, 201
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
